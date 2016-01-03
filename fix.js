7e6513bf0856e184b5419a6ff8645e8083c69202
Ken Wilcox <wilcoxk@gmail.com>
2016-01-03 15:51:35 -0700

    Initial Commit

---
+++ fix.js
@@ -0,0 +1,16 @@
+// Nothing major, just adding some things that should have been there
+
+Function.prototype.method = function (name, func) {
+    //if (!this.prototype[name]) {
+      this.prototype[name] = func;
+    //}
+    return this;
+};
+
+Number.method('integer', function () {
+    return Math[this < 0 ? 'ceil': 'floor'](this);
+});
+
+String.method('trim', function () {
+   return this.replace(/^\s+|\s+$/g, ''); 
+});
